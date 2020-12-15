232da41ade2f9121157a0644d8802e0a0b1893b5
Michael Pritchard <michael.pritchard@deltafs.co.uk>
2020-12-15 17:43:02 +0000

    module 6 upto exercie 3 task 2

---
+++ Allfiles/Labs/06/Starter/GraphClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Graph;    
+using Microsoft.Graph.Auth;
+
+namespace GraphClient
+{
+    public class Program
+    {
+        private const string _clientId = "ca4c5ab0-4eec-44f4-ac03-548d5bbd4ce3";
+        private const string _tenantId = "38eb2ed4-8553-4a58-80fb-03cad1623f3a";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+            var scopes = new string[]
+            {
+                "user.read"
+            };
+            var result = await app
+                .AcquireTokenInteractive(scopes)
+                .ExecuteAsync();
+            await Console.Out.WriteLineAsync($"Token:\t{result.AccessToken}");
+        }
+    }
+}
